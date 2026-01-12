10ce51db848310c23c3b3919525a6dcb58b78b99
Safspark-dotcom <racwaree1@gmail.com>
2026-01-12 08:31:09 +0700

    Update Bladeswitch.h

--- Bladeswitch.h
+++ Bladeswitch.h
@@ -20,8 +20,6 @@ const unsigned int maxLedsPerStrip = 144;
 #define NO_REPEAT_RANDOM
 #define COLOR_CHANGE_DIRECT
 #define BLADE_DETECT_PIN blade4Pin
-#define FETT263_LOCKUP_DELAY 200
-#define FETT263_BM_CLASH_DETECT 6
 #endif
 
 #ifdef CONFIG_PROP
@@ -42,10 +40,49 @@ StyleNormalPtr<CYAN, WHITE, 300, 800>(),
 
 };
 
+Preset presets2[] = {
+{ "font0;common",  "font0/tracks/track.wav",
+StyleNormalPtr<CYAN, WHITE, 300, 800>(),
+
+StyleNormalPtr<CYAN, WHITE, 300, 800>(),
+
+"preset0"
+},
+
+};
+
+Preset presets1[] = {
+{ "font0;common",  "font0/tracks/track.wav",
+StyleNormalPtr<CYAN, WHITE, 300, 800>(),
+
+StyleNormalPtr<CYAN, WHITE, 300, 800>(),
+
+"preset0"
+},
+
+};
+
 BladeConfig blades[] = {
-{ 0, WS281XBladePtr<132, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(), 
+{ 0, 
+//BLADE
+WS281XBladePtr<132, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(), 
+//MOTOR
 SimpleBladePtr<CreeXPE2WhiteTemplate<550>, NoLED, NoLED, NoLED, bladePowerPin1, -1, -1, -1>(), 
 CONFIGARRAY(presets) },
+
+{ 1, 
+//BLADE
+WS281XBladePtr<132, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(), 
+//MOTOR
+SimpleBladePtr<CreeXPE2WhiteTemplate<550>, NoLED, NoLED, NoLED, bladePowerPin1, -1, -1, -1>(), 
+CONFIGARRAY(presets1) },
+
+{ 2, 
+//BLADE
+WS281XBladePtr<132, bladePin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(), 
+//MOTOR
+SimpleBladePtr<CreeXPE2WhiteTemplate<550>, NoLED, NoLED, NoLED, bladePowerPin1, -1, -1, -1>(), 
+CONFIGARRAY(presets2) },
 
 };
 #endif
